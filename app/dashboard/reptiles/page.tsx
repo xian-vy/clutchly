388816275bdf642a9fe29661be6065cb2023cfc3
jovie00 <xianvy0000@gmail.com>
2025-04-11 10:43:11 +0800

    Style : Reptile form - Add option button, revamp tab design, create new button variant

--- app/dashboard/reptiles/page.tsx
+++ app/dashboard/reptiles/page.tsx
@@ -3,20 +3,30 @@
 import { MorphsTab } from '@/components/dashboard/reptiles/morphs/MorphsTab';
 import { ReptilesTab } from '@/components/dashboard/reptiles/reptiles/ReptilesTab';
 import { SpeciesTab } from '@/components/dashboard/reptiles/species/SpeciesTab';
+import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Settings } from 'lucide-react';
 
 export default function ReptilesPage() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-6">Reptile Management</h1>
-
+    <div className="container mx-auto">
+        <div className="flex items-center justify-between w-full">
+            <h1 className="text-3xl font-bold mb-6">Reptile Management</h1>
+            <Button variant="primary">
+              <Settings className="h-4 w-4" />
+              Options
+            </Button>
+        </div>
       <Tabs defaultValue="reptiles" className="space-y-6">
-        <TabsList>
-          <TabsTrigger value="reptiles">Reptiles</TabsTrigger>
-          <TabsTrigger value="species">Species</TabsTrigger>
-          <TabsTrigger value="morphs">Morphs</TabsTrigger>
-        </TabsList>
-
+        <div className="flex flex-col w-full">
+            <TabsList>
+              <TabsTrigger value="reptiles">Reptiles</TabsTrigger>
+              <TabsTrigger value="species">Species</TabsTrigger>
+              <TabsTrigger value="morphs">Morphs</TabsTrigger>
+            </TabsList>
+            <hr className='mt-[1px]'/>
+        </div>
+    
         <TabsContent value="reptiles">
           <ReptilesTab />
         </TabsContent>
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -26,7 +26,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        "bg-transparent text-muted-foreground inline-flex gap-2 h-9 w-fit items-center justify-center rounded-lg ",
         className
       )}
       {...props}
@@ -42,8 +42,8 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        className
+        className="bg-muted inline-flex items-center justify-center whitespace-nowrap py-1 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  relative h-9 rounded-none border-b-[3px] border-b-transparent  px-4 pb-3 pt-2 font-medium text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-primary data-[state=active]:shadow-none uppercase",      
+          className
       )}
       {...props}
     />
