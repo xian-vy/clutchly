32ba6fad26a2aa032d1fa21df7de81ba7635824a
xianvy <xianvy0000@gmail.com>
2025-04-16 22:21:36 +0800

    Feat : Add no het and visual traits indicator

--- components/dashboard/breeding/breeding/BreedingProjectDetails.tsx
+++ components/dashboard/breeding/breeding/BreedingProjectDetails.tsx
@@ -160,18 +160,24 @@ export function BreedingProjectDetails({
           {/* Parents Info */}
           <div className="flex items-start justify-center gap-10 2xl:gap-16">
               <div className='flex flex-col items-center gap-1'>
-                    <p className='text-sm text-center  flex gap-1 font-bold'>
+                    <p className='text-sm text-center  flex gap-1 font-semibold'>
                         {reptileMap.get(project.male_id)?.name || 'Unknown'}
                         <Mars className="h-4 w-4 text-blue-400"/>
                     </p>
-                    <p className='text-sm text-center text-muted-foreground'> {reptileMap.get(project.male_id)?.morphName}</p>
+                    <p className='text-xs text-center '> {reptileMap.get(project.male_id)?.morphName}</p>
                     <div className='space-y-1'>
                         <div className="flex items-center justify-center gap-1.5">
+                            {reptileMap.get(project.male_id)?.visuals?.length === 0  &&
+                              <span className='text-xs text-red-500 dark:text-red-300'>No Visual Traits</span>
+                            }
                             {reptileMap.get(project.male_id)?.visuals?.map((visualtrait, index) =>
                               <Badge key={index}  variant="secondary" className='text-[0.7rem]'>{visualtrait}</Badge>
                             )}
                         </div>
                         <div className="flex  items-center justify-center gap-1.5">
+                            {reptileMap.get(project.male_id)?.hets?.length === 0  &&
+                              <span className='text-xs text-red-500 dark:text-red-300'>No Het Traits</span>
+                            }
                             {reptileMap.get(project.male_id)?.hets?.map((het, index) =>
                               <Badge key={index}  variant="secondary"  className='text-[0.7rem]'>{het.percentage} {" % ph "}{het.trait}</Badge>
                             )}
@@ -182,18 +188,24 @@ export function BreedingProjectDetails({
                  <X className='text-muted-foreground'/>
               </div>                        
               <div  className='flex flex-col items-center  gap-1'>
-                    <p className='text-sm flex gap-1 font-bold'>
+                    <p className='text-sm flex gap-1 font-semibold'>
                         {reptileMap.get(project.female_id)?.name || 'Unknown'}
                         <Venus className="h-4 w-4 text-red-500"/>
                     </p>
-                    <p className='text-sm text-center text-muted-foreground'> {reptileMap.get(project.female_id)?.morphName} </p>
+                    <p className='text-xs text-center'> {reptileMap.get(project.female_id)?.morphName} </p>
                     <div className='space-y-1'>
                       <div className="flex items-center justify-center gap-1.5">
+                          {reptileMap.get(project.female_id)?.visuals?.length === 0  &&
+                            <span className='text-xs text-red-500 dark:text-red-300'>No Visual Traits</span>
+                          }
                           {reptileMap.get(project.female_id)?.visuals?.map((visualtrait, index) =>
                             <Badge key={index}  variant="secondary" className='text-[0.7rem]'>{visualtrait}</Badge>
                           )}
                       </div>
                       <div className="flex  items-center justify-center gap-1.5">
+                         {reptileMap.get(project.female_id)?.hets?.length === 0  &&
+                            <span className='text-xs text-red-500 dark:text-red-300'>No Het Traits</span>
+                          }
                           {reptileMap.get(project.female_id)?.hets?.map((het, index) =>
                             <Badge key={index}  variant="secondary"  className='text-[0.7rem]'>{het.percentage} {" % ph "}{het.trait}</Badge>
                           )}
