e97b7de771bc1b97cbbb53cb264d391761fb21ee
jovie00 <xianvy0000@gmail.com>
2025-04-21 16:49:54 +0800

    Style (table) : Adjust text

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -12,7 +12,7 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
     >
       <table
         data-slot="table"
-        className={cn("w-full caption-bottom text-sm", className)}
+        className={cn("w-full caption-bottom text-xs md:text-[0.8rem] 3xl:text-sm", className)}
         {...props}
       />
     </div>
