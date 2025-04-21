d6dce3318de2685c37a2099b35eb032d78c04259
jovie00 <xianvy0000@gmail.com>
2025-04-21 14:42:54 +0800

    Style : adjust h1 size

--- app/(dashboard)/breeding/page.tsx
+++ app/(dashboard)/breeding/page.tsx
@@ -9,7 +9,7 @@ export default async function BreedingPage() {
   return (
     <div className="container mx-auto">
       <div className="flex items-center justify-between w-full">
-        <h1 className="text-3xl font-bold mb-6">Breeding Management</h1>
+        <h1 className="text-lg sm:text-xl 2xl:text-2xl 3xl:text-3xl font-bold mb-6">Breeding Management</h1>
         <Button variant="outline">
           <Settings className="h-4 w-4" />
           Options
