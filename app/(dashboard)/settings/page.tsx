caab58820946b014f5b94973344927a9d5193442
xianvy <xianvy0000@gmail.com>
2025-04-27 09:57:26 +0800

    Feat (settings) : setup initial page

---
+++ app/(dashboard)/settings/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Settings } from 'lucide-react';
+
+export default async function SettingsPage() {
+
+  return (
+    <div className="container mx-auto">
+        <div className="flex items-center justify-between w-full mb-3 lg:mb-4 xl:mb-6">
+            <h1 className="text-lg sm:text-xl 2xl:text-2xl 3xl:text-3xl font-bold">Settings</h1>
+            <Button size="sm" variant="outline">
+              <Settings className="h-4 w-4" />
+              Options
+            </Button>
+        </div>
+      <Tabs defaultValue="profile" className="space-y-2 md:space-y-3 xl:space-y-6">
+        <div className="flex flex-col w-full">
+            <TabsList>
+              <TabsTrigger value="profile">Profile</TabsTrigger>
+              <TabsTrigger value="system">System</TabsTrigger>
+            </TabsList>
+            <hr className='mt-[1px]'/>
+        </div>
+    
+        <TabsContent value="profile">
+        </TabsContent>
+
+        <TabsContent value="system">
+        </TabsContent>
+
+       
+      </Tabs>
+    </div>
+  )
+} 
