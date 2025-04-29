a51a0bdae7ad2329739cfd4652d6813e514747fa
jovie00 <xianvy0000@gmail.com>
2025-04-29 13:24:35 +0800

    Style (sidebar) : increase 3xl height

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -105,7 +105,7 @@ export function Navigation() {
             </div>
           )}
         </div>
-        <ScrollArea className='h-[85vh]'>
+        <ScrollArea className='h-[85vh] 3xl:h-[90vh]'>
           <nav className="px-3 2xl:px-4 space-y-4 2xl:space-y-5 pt-2 3xl:pt-3 flex-1">
             {Object.entries(groupedNavItems).map(([section, items]) => (
               <div key={section} className="space-y-1">
