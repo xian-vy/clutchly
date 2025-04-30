1d02e44cbea459896ee5f14580907d5c141f6cea
jovie00 <xianvy0000@gmail.com>
2025-04-30 11:00:34 +0800

    Feat (genetic calc history) : add morph and reptile name

--- components/dashboard/genetic-calculator/GeneticHistoryTab.tsx
+++ components/dashboard/genetic-calculator/GeneticHistoryTab.tsx
@@ -5,11 +5,20 @@ import { Card } from '@/components/ui/card'
 import { GeneticCalculation } from '@/lib/types/genetic-calculator'
 import { format } from 'date-fns'
 import { getGeneticCalculations } from '@/app/api/genetic-calculations/history'
+import { Reptile } from '@/lib/types/reptile'
+import { getReptiles } from '@/app/api/reptiles/reptiles'
+import { useMorphsStore } from '@/lib/stores/morphsStore'
 
 const GeneticHistoryTab = () => {
+    const {morphs} = useMorphsStore()
+
   const { data: history, isLoading } = useQuery<GeneticCalculation[]>({
     queryKey: ['genetic-calculations-history'],
     queryFn: getGeneticCalculations,
+  });
+  const { data: reptiles = [] } = useQuery<Reptile[]>({
+    queryKey: ['reptiles'],
+    queryFn: getReptiles,
   });
 
   if (isLoading) {
@@ -27,10 +36,16 @@ const GeneticHistoryTab = () => {
       </div>
     );
   }
+  
 
   return (
     <div className="space-y-4">
-      {history.map((calculation) => (
+      {history.map((calculation) => {
+        const damInfo = reptiles.find((reptile) => reptile.id === calculation.dam_id);
+        const sireInfo = reptiles.find((reptile) => reptile.id === calculation.sire_id);
+        const damMorph =morphs.find((m) => m.id.toString() === damInfo?.morph_id.toString());
+        const sireMorph =  morphs.find((m) => m.id.toString() === sireInfo?.morph_id.toString());
+        return(
         <Card key={calculation.id} className="p-4">
           <div className="flex justify-between items-start mb-4">
             <div>
@@ -47,11 +62,13 @@ const GeneticHistoryTab = () => {
               <div className="grid grid-cols-2 gap-4">
                 <div>
                   <p className="text-sm font-medium">Dam</p>
-                  <p className="text-sm text-muted-foreground">ID: {calculation.dam_id}</p>
+                  <p className="text-sm text-muted-foreground">{damInfo?.name + " " + damInfo?.reptile_code}</p>
+                  <p className="text-sm text-muted-foreground">{damMorph?.name}</p>
                 </div>
                 <div>
                   <p className="text-sm font-medium">Sire</p>
-                  <p className="text-sm text-muted-foreground">ID: {calculation.sire_id}</p>
+                  <p className="text-sm text-muted-foreground">{sireInfo?.name + " " + sireInfo?.reptile_code}</p>
+                    <p className="text-sm text-muted-foreground">{sireMorph?.name}</p>
                 </div>
               </div>
             </div>
@@ -83,7 +100,7 @@ const GeneticHistoryTab = () => {
             </div>
           </div>
         </Card>
-      ))}
+      )})}
     </div>
   )
 }
