4369a497c6aa22a95ef379655850f5ac992060f0
jovie00 <xianvy0000@gmail.com>
2025-05-08 17:05:43 +0800

    Feat (sidebar) : use dvh

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -93,7 +93,7 @@ export function Navigation() {
       {/* Sidebar */}
       <div
         className={cn(
-          "fixed flex flex-col h-[100vh] inset-y-0 left-0 z-40 bg-sidebar border-r border-sidebar-border transform transition-all duration-200 ease-in-out lg:translate-x-0",
+          "fixed flex flex-col h-[100dvh] inset-y-0 left-0 z-40 bg-sidebar border-r border-sidebar-border transform transition-all duration-200 ease-in-out lg:translate-x-0",
           isSidebarOpen ? "translate-x-0" : "-translate-x-full",
           isCollapsed ? "w-16" : "w-[18rem] 3xl:w-[19rem]"
         )}
