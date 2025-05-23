4890f6b0b9032c6480c002cac344fb03d72a1110
xianvy <xianvy0000@gmail.com>
2025-05-23 22:24:28 +0800

    Fix : sitemap error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,22 @@
-import { APP_URL } from "@/lib/constants/app";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const BASE_URL = "https://clutchly.vercel.app"
   const staticRoutes: MetadataRoute.Sitemap = [
     {
-      url: APP_URL,
+      url: BASE_URL,
       lastModified: new Date().toISOString(),
       changeFrequency: 'weekly' as const,
       priority: 1.0
     },
     {
-      url: `${APP_URL}/auth/signin`,
+      url: `${BASE_URL}/auth/signin`,
       lastModified: new Date().toISOString(),
       changeFrequency: 'monthly' as const,
       priority: 0.8
     },
     {
-      url: `${APP_URL}/auth/signup`,
+      url: `${BASE_URL}/auth/signup`,
       lastModified: new Date().toISOString(),
       changeFrequency: 'monthly' as const,
       priority: 0.8
